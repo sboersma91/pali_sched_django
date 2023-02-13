4d6b021f41cd6eda55c66a3a61cf5700748af6cd
sboersma91 <sboersma91@gmail.com>
2023-02-12 17:33:26 -0800

    login vid done

--- scheduler_project/members/views.py
+++ scheduler_project/members/views.py
@@ -3,6 +3,17 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method=="POST":
+        username =  request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
 
-# Create your views here.
+        if user is not None:
+            login(request, user)
+            return redirect('home_pay')
+            # redirect is page to send them to.
+        else:
+            messages.success(request, "There was an error")
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
