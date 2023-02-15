ba8e65bc21f9f75fec6e130018303eee1f09349e
Scott B <70726172+sboersma91@users.noreply.github.com>
2023-02-15 00:04:41 -0800

    User authentication app (#6)
    * django_wed_21-940
    
    * 12min in vid
    
    * login vid done
    
    * logout vid
    
    * Register new user start
    
    * watch vid 25 next
    
    * add git ignore
    
    * massive stuff here
    
    * adding sched virt to git ignore

--- scheduler_project/scheduler_app/models.py
+++ scheduler_project/scheduler_app/models.py
@@ -337,3 +337,15 @@ class TheSched(models.Model):
         return self.sched 
 
 
+class Instructor(models.Model):
+    fname = CharField(max_length=450)
+    lname = CharField(max_length=450)
+    ropes_lead = BooleanField()
+    school_lead = BooleanField()
+    # days_incabin = SmallIntegerField()
+    # this will have to be a calculated field.
+    cpr = BooleanField(choices=(("fr", "fresh"), ('hr', 'house'),('boolean field','boolean field')))
+    firstaid = CharField(choices= (('yes','yes'),('jack','jack'), ('charfield','charfield')),max_length=100)
+    
+    def __str__(self):
+        return self.fname + ' ' + self.lname
